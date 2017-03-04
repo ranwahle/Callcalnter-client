593d49fd93c16519ee8569f00756d139e6ac66e8
ranwahle <ran.wahle@gmail.com>
2017-03-04 07:33:12 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CallerScreenComponent } from './components/caller-screen/caller-screen.component';
+import { RepresentativeScreenComponent } from './components/representative-screen/representative-screen.component';
+import { ManagerScreenComponent } from './components/manager-screen/manager-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CallerScreenComponent,
+    RepresentativeScreenComponent,
+    ManagerScreenComponent
   ],
   imports: [
     BrowserModule,
